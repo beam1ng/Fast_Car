559ac6e213a4d4d40118aa34558c20d821987aa0
Kuba <jakub.augustyniak1.618@gmail.com>
2021-02-23 01:03:11 +0100

    message

---
+++ Assets/Scripts/Time_update.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Time_update : MonoBehaviour
+{
+    public TextMeshProUGUI time_counter;
+    public bool racing = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (racing)
+        {
+            float tmp = Time.timeSinceLevelLoad;
+            time_counter.text = tmp.ToString("0.000");
+
+        }
+    }
+}
